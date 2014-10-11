ce6b46f69973cf12cb9a15b19821db8f963392a9
Ville Korhonen <ville@xd.fi>
2014-10-11 03:03:26 +0300

    Replace sources.list with http.d.n.

--- provision/999-last.sh
+++ provision/999-last.sh
@@ -9,6 +9,15 @@ rm -f /var/lib/apt/lists/*
 # Remove installation logs
 rm -rf /var/log/installer
 
+cat > /etc/apt/sources.list << EOF
+deb http://http.debian.net/debian jessie main contrib non-free
+deb-src http://http.debian.net/debian jessie main contrib non-free
+deb http://http.debian.net/debian jessie-updates main contrib non-free
+deb-src http://http.debian.net/debian jessie-updates main contrib non-free
+deb http://security.debian.org/ jessie/updates main contrib non-free
+deb-src http://security.debian.org/ jessie/updates main contrib non-free
+EOF
+
 # Zero out the free space to save space in the final image:
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
