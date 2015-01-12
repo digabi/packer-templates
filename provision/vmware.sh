8061640fa4f99d6df138efdfc22c079a141162c0
Ville Korhonen <ville.korhonen@ylioppilastutkinto.fi>
2015-01-12 17:47:03 +0200

    Fix upload path of VMware tools.

--- provision/vmware.sh
+++ provision/vmware.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-mount /media/cdrom
+mount -o loop -t iso9660 /opt/vmware-tools.iso /media/cdrom
+#mount /media/cdrom
 
 TEMPDIR="$(mktemp -d)"
 
@@ -11,3 +12,6 @@ cd ${TEMPDIR}
 tar xzf /media/cdrom/VMwareTools-*.tar.gz
 
 ./vmware-tools-distrib/vmware-install.pl default
+
+umount -l /media/cdrom
+rm -f /opt/vmware-tools.iso
