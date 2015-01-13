3a8645b8f2bca99841fa843995829c7987d6504a
Ville Korhonen <ville.korhonen@ylioppilastutkinto.fi>
2015-01-13 09:19:01 +0200

    Fix paths to VMware Tools installer.

--- provision/vmware.sh
+++ provision/vmware.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-mount -o loop -t iso9660 /opt/vmware-tools.iso /media/cdrom
+mount -o loop -t iso9660 /home/vagrant/linux.iso /media/cdrom
 #mount /media/cdrom
 
 TEMPDIR="$(mktemp -d)"
@@ -14,4 +14,4 @@ tar xzf /media/cdrom/VMwareTools-*.tar.gz
 ./vmware-tools-distrib/vmware-install.pl default
 
 umount -l /media/cdrom
-rm -f /opt/vmware-tools.iso
+rm -f /home/vagrant/linux.iso
