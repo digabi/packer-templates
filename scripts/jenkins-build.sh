4f9c7f49288874610a34f53e26d443bb54229db7
Ville Korhonen <ville.korhonen@ylioppilastutkinto.fi>
2015-01-13 14:40:41 +0200

    Check if previous VM exists, try to destroy if it does.

--- scripts/jenkins-build.sh
+++ scripts/jenkins-build.sh
@@ -8,7 +8,15 @@ cleanup() {
 
 trap cleanup EXIT
 
-vboxmanage unregistervm --delete "debian-jessie-amd64" || echo "I: No previous VM."
+VM_NAME="debian-jessie-amd64"
+VM_UUID="$(vboxmanage list vms |grep ^\"${VM_NAME}\" |cut -d' ' -f1)"
+
+if [ "${VM_UUID}" != "" ]
+then
+    echo "I: Previous VM exists (${VM_UUID}), trying to poweroff & destroy..."
+    vboxmanage controlvm "${VM_UUID}" poweroff
+    vboxmanage unregistervm --delete "${VM_UUID}"
+fi
 
 make clean
 make build BUILD_ID=${BUILD_ID:-$(date +%Y-%m%-d_%H-%M-%S)}.${BUILD_NUMBER:-0} DEBIAN_MIRROR=${DEBIAN_MIRROR:-http://http.debian.net/debian}
