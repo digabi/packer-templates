d1c283c7abafd4e71e0b83b38bca083472d46d48
Ville Korhonen <ville.korhonen@ylioppilastutkinto.fi>
2015-01-14 13:07:48 +0200

    Check if system has support for VMware boxes before importing.

--- scripts/jenkins-import.sh
+++ scripts/jenkins-import.sh
@@ -17,13 +17,19 @@ do
     fi
     PROVIDER="$(echo ${box} |cut -d_ -f3)"
     echo "I: Add box ${box} to vagrant using provider ${PROVIDER}, with name ${VAGRANT_BOX_NAME}..."
+
     case "${PROVIDER}" in
         virtualbox)
             ${VAGRANT} box add --force --provider=${PROVIDER} ${VAGRANT_BOX_NAME} ${box}
         ;;
         vmware)
-            PROVIDER="${PROVIDER}_desktop"
-            ${VAGRANT} box add --force --provider=${PROVIDER} ${VAGRANT_BOX_NAME} ${box}
+            if [ "$(${VAGRANT} plugin list |grep vmware)" != "" ]
+            then
+                PROVIDER="${PROVIDER}_desktop"
+                ${VAGRANT} box add --force --provider=${PROVIDER} ${VAGRANT_BOX_NAME} ${box}
+            else
+                echo "W: No support for VMware boxes, not adding ${box}."
+            fi
         ;;
         *)
             echo "W: Unsupported provider ${PROVIDER}."
