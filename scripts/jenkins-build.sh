b13d7e9752113487eb5269b64d2efa5c4e603b4e
Ville Korhonen <ville.korhonen@ylioppilastutkinto.fi>
2015-01-14 11:56:05 +0200

    Fix BUILD_ONLY to match Jenkins configuration.

--- scripts/jenkins-build.sh
+++ scripts/jenkins-build.sh
@@ -30,9 +30,9 @@ make clean
 make build BUILD_ID=${BUILD_ID:-$(date +%Y-%m%-d_%H-%M-%S)}.${BUILD_NUMBER:-0} DEBIAN_MIRROR=${DEBIAN_MIRROR:-http://http.debian.net/debian}
 
 VAGRANT="/usr/bin/vagrant"
-BUILD_ONLY="${BUILD_ONLY:-0}"
+BUILD_ONLY="${BUILD_ONLY:-false}"
 
-if [ "${BUILD_ONLY}" != "0" ]
+if [ "${BUILD_ONLY}" != "false" ]
 then
     echo "I: Doing only build, stopping here."
     exit 0
