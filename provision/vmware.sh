7e0e36ab84e3a25de49dd53f2ca0fb0c04c42ffa
Ville Korhonen <ville@xd.fi>
2015-02-09 05:57:36 +0200

    Install VMware tools using vmware-tools-patches.
    Currently building VMware HGFS modules fails on kernel 3.16.x,
    so let's use cool patches to fix builds.
    
    We are using specific commit (3a392...), to prevent new
    code from running without the knowledge of sysadmin.
    
    Signed-off-by: Ville Korhonen <ville@xd.fi>

--- provision/vmware.sh
+++ provision/vmware.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+apt-get -y install git
+
 mount -o loop -t iso9660 /home/vagrant/linux.iso /media/cdrom
 #mount /media/cdrom
 
@@ -9,10 +11,16 @@ OLDDIR="${PWD}"
 TEMPDIR="$(mktemp -d)"
 
 cd ${TEMPDIR}
+git clone https://github.com/rasa/vmware-tools-patches.git vmware-tools-patches
+cd vmware-tools-patches
+git checkout 3a392a590b873bfeb515a36240c2d1510e87675c
 
-tar xzf /media/cdrom/VMwareTools-*.tar.gz
+cp /media/cdrom/VMwareTools-*.tar.gz .
+#tar xzf /media/cdrom/VMwareTools-*.tar.gz
 
-./vmware-tools-distrib/vmware-install.pl default
+#./vmware-tools-distrib/vmware-install.pl default
+
+./untar-and-patch-and-compile.sh
 
 umount -l /media/cdrom
 rm -f /home/vagrant/linux.iso
