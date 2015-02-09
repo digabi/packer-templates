b784c1761ba67c4ebab3f34c04e0de9406d4e8b7
Ville Korhonen <ville@xd.fi>
2015-02-09 06:55:59 +0200

    Drop support for proprietary VMware tools.
    We are now using open-sourced open-vm-tools.
    
    Signed-off-by: Ville Korhonen <ville@xd.fi>

--- provision/vmware.sh
+++ provision/vmware.sh
@@ -4,33 +4,3 @@ set -e
 
 apt-get -y install open-vm-tools open-vm-tools-dkms
 
-exit 0
-#
-# We're never running this:
-#
-
-apt-get -y install git
-
-mount -o loop -t iso9660 /home/vagrant/linux.iso /media/cdrom
-#mount /media/cdrom
-
-OLDDIR="${PWD}"
-TEMPDIR="$(mktemp -d)"
-
-cd ${TEMPDIR}
-git clone https://github.com/rasa/vmware-tools-patches.git vmware-tools-patches
-cd vmware-tools-patches
-git checkout 3a392a590b873bfeb515a36240c2d1510e87675c
-
-cp /media/cdrom/VMwareTools-*.tar.gz .
-#tar xzf /media/cdrom/VMwareTools-*.tar.gz
-
-#./vmware-tools-distrib/vmware-install.pl default
-
-./untar-and-patch-and-compile.sh
-
-umount -l /media/cdrom
-rm -f /home/vagrant/linux.iso
-
-cd "${OLDDIR}"
-rm -rf "${TEMPDIR}"
