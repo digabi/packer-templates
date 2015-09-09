661fec301286777297d57244235c3362ac988687
Timo Metsälä <timo.metsala@reaktor.fi>
2015-09-09 10:44:24 +0300

    Create metadata.json for simple private packer cloud management

--- scripts/jenkins-build.sh
+++ scripts/jenkins-build.sh
@@ -25,3 +25,22 @@ case "${BITS}" in
         exit 1
 esac
 make build BUILD_ID=$(date +%Y-%m%-d_%H-%M-%S).${BUILD_NUMBER:-0} DEBIAN_MIRROR=${DEBIAN_MIRROR:-http://http.debian.net/debian} CONFIG_PACKER=${CONFIG_FILE}
+BOX_VERSION=${BUILD_NUMBER:-0}
+for box in *.box
+do
+    case "${box}" in
+        *_vmware_*)
+            VMWARE_BOX_URL=${BUILD_URL}/artifact/${box}
+            VMWARE_BOX_SHA1=$(sha1sum ${box} | awk '{print $1}')
+            sed -i.bak -e "s|__BOX_VERSION__|${BOX_VERSION}|g" -e "s|__VMWARE_BOX_URL__|${VMWARE_BOX_URL}|g" -e "s|__VMWARE_BOX_SHA1__|${VMWARE_BOX_SHA1}|g" metadata.json
+            ;;
+        *_virtualbox_*)
+            VBOX_BOX_URL=${BUILD_URL}/artifact/${box}
+            VBOX_BOX_SHA1=$(sha1sum ${box} | awk '{print $1}')
+            sed -i.bak -e "s|__BOX_VERSION__|${BOX_VERSION}|g" -e "s|__VBOX_BOX_URL__|${VBOX_BOX_URL}|g" -e "s|__VBOX_BOX_SHA1__|${VBOX_BOX_SHA1}|g" metadata.json
+            ;;
+        *)
+            echo "Unknown box ${box}"
+            exit 1
+    esac
+done
