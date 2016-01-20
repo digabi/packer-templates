9db202899c084fd2c1e2ab83c9a13ebf490f7f3c
Timo Metsälä <timo.metsala@reaktor.fi>
2016-01-20 12:12:30 +0200

    Install finnish locale for postgres' needs

--- provision/800-digabi.sh
+++ provision/800-digabi.sh
@@ -210,11 +210,15 @@ export DEBIAN_FRONTEND="noninteractive"
 
 apt-get update
 
-apt-get -y -o "Acquire::http::Pipeline-Depth=10" install digabi-dev ruby-dev zip npm nodejs nginx libpq-dev libnss3-tools git rsync curl unzip ruby parallel uuid-runtime netcat-traditional vlc postgresql-9.5 postgresql-contrib-9.5
+apt-get -y -o "Acquire::http::Pipeline-Depth=10" install digabi-dev ruby-dev zip npm nodejs nginx libpq-dev libnss3-tools git rsync curl unzip ruby parallel uuid-runtime netcat-traditional vlc locales postgresql-9.5 postgresql-contrib-9.5
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.1/install.sh | sudo -u vagrant bash
 sudo -u vagrant /bin/bash -c ". ~/.nvm/nvm.sh; nvm install 4.2.1"
 
 sudo update-alternatives --install /usr/bin/node node /usr/bin/nodejs 10
+
+echo "I: Install finnish locale.."
+sed -i.bak -e '/fi_FI.UTF-8/s/# //' /etc/locale.gen
+locale-gen
 
 echo "I: Configure postgresql.."
 su postgres -c "createuser -d vagrant" || true
