413f47ab98bcbe00f211bba29292ea2ba437d1c3
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2016-05-09 11:00:39 +0300

    Install Node 4.4.4 instead of 4.4.3

--- provision/800-digabi.sh
+++ provision/800-digabi.sh
@@ -212,7 +212,7 @@ apt-get update
 
 apt-get -y -o "Acquire::http::Pipeline-Depth=10" install digabi-dev ruby-dev zip npm nodejs nginx libpq-dev libnss3-tools git rsync curl unzip ruby parallel uuid-runtime netcat-traditional vlc locales postgresql-9.5 postgresql-contrib-9.5
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.1/install.sh | sudo -u vagrant bash
-sudo -u vagrant /bin/bash -c ". ~/.nvm/nvm.sh; nvm install 4.2.1; nvm install 4.4.3"
+sudo -u vagrant /bin/bash -c ". ~/.nvm/nvm.sh; nvm install 4.2.1; nvm install 4.4.4;"
 
 sudo update-alternatives --install /usr/bin/node node /usr/bin/nodejs 10
 
