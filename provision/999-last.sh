e1a86a81ab9545044a908563f1df7bb651286f3f
Pasi Huhtiniemi <pasi.huhtiniemi@reaktor.fi>
2019-04-25 12:01:50 +0300

    Move vivaldi -> jenkins.local

--- provision/999-last.sh
+++ provision/999-last.sh
@@ -10,9 +10,9 @@ rm -f /var/lib/apt/lists/*
 rm -rf /var/log/installer
 
 cat > /etc/apt/sources.list << EOF
-deb http://192.168.2.49/debian stretch main contrib non-free
-deb http://192.168.2.49/debian-security stretch/updates main contrib non-free
-deb http://192.168.2.49/debian stretch-updates main contrib non-free
+deb http://192.168.3.32/debian stretch main contrib non-free
+deb http://192.168.3.32/debian-security stretch/updates main contrib non-free
+deb http://192.168.3.32/debian stretch-updates main contrib non-free
 EOF
 
 echo "I: DHCP hack to keep vmware/vagrant up to date"
